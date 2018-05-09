e02ab88db5523c6b2a28a73a404efdb89d462dfc
Jennifer CHARLOIS <jcharloi@e1r3p9.42.fr>
2018-05-09 15:37:35 +0200

    ex01 done

---
+++ ex02/script.js
@@ -0,0 +1,68 @@
+class geographie {
+
+	constructor() {
+		this.countries = [
+		{
+			'country_name': "france",
+			'continent': "europe",
+			'english': false
+		}
+		]
+		this.cities = [
+		{
+			'city_name': "paris",
+			'country': "france"
+		}
+	 	]
+	}
+	insert_country(country) {
+		this.countries.push(country);
+	}
+	insert_city(city) {
+		this.cities.push(city);
+	}
+	get_english_speaking_countries() {
+		var ret = this.countries.map(function(country) {
+			if (country.english)
+				return country.country_name;
+		})
+		return ret.sort();
+	}
+	get_countries_by_continent(continent) {
+		var ret = this.countries.map(function(country) {
+			if (country.continent == continent)
+				return country.country_name;
+		})
+		return ret.sort();
+	}
+	get_cities_by_continent(continent) {
+		var countriesArray = this.get_countries_by_continent(continent);
+		
+		var ret = this.cities.map(function(city) {
+			countriesArray.foreach(function(country) {
+				if (city.country == country.country_name)
+					return city.city_name;
+			})
+		})
+		return ret.sort();
+	}
+	get_english_speaking_cities() {
+		var countriesArray =  this.get_english_speaking_countries()
+		
+		var ret = this.cities.map(function(city) {
+			countriesArray.foreach(function(country) {
+				if (city.country == country.country_name)
+					return city.city_name;
+			})
+		})
+		return ret.sort();
+	}
+}
+
+var geo = new geographie();
+
+function add_place() {
+	var place = prompt("Quel endroit souhaitez-vous ajouter ?");
+
+	console.log(place);
+}

5218f874be37f5889738723a09f18b6a12418dd1
Jennifer CHARLOIS <jcharloi@e1r3p9.42.fr>
2018-05-11 13:24:59 +0200

    finished

--- ex01/class.js
+++ ex01/class.js
@@ -1,60 +1,75 @@
 class geographie {
-
 	constructor() {
 		this.countries = [
 		{
-        	'country_name': "france",
-        	'continent': "europe",
-        	'english': false
-    	}
-    	]
-    	this.cities = [
-    	{
-    		'city_name': "paris",
-    		'country': "france"
-    	}
-   	 	]
+			'country_name': "france",
+			'continent': "europe",
+			'english': false
+		}
+		]
+		this.cities = [
+		{
+			'city_name': "paris",
+			'country': "france"
+		}
+	 	]
 	}
-	insert_country(country) {
+	insert_country(country)
+	{
 		this.countries.push(country);
 	}
-	insert_city(city) {
+	insert_city(city)
+	{
 		this.cities.push(city);
 	}
-	get_english_speaking_countries() {
-		var ret = this.countries.map(function(country) {
+	get_english_speaking_countries()
+	{
+		var ret = this.countries.map(function(country)
+		{
 			if (country.english)
 				return country.country_name;
 		})
-		return ret.sort();
+		return ret.sort().filter(function(elem) { return elem != undefined });
 	}
-	get_countries_by_continent(continent) {
-		var ret = this.countries.map(function(country) {
+	get_countries_by_continent(continent)
+	{
+		var ret = this.countries.map(function(country)
+		{
 			if (country.continent == continent)
 				return country.country_name;
 		})
-		return ret.sort();
+		return ret.sort().filter(function(elem) { return elem != undefined });
 	}
-	get_cities_by_continent(continent) {
+	get_cities_by_continent(continent)
+	{
 		var countriesArray = this.get_countries_by_continent(continent);
-		
-		var ret = this.cities.map(function(city) {
-			countriesArray.foreach(function(country) {
-				if (city.country == country.country_name)
-					return city.city_name;
+
+		var ret = this.cities.map(function(city)
+		{
+			var newCity;
+			countriesArray.forEach(function(country)
+			{
+				if (city.country == country)
+					newCity = city.city_name;
 			})
+			return newCity;
 		})
-		return ret.sort();
+		return ret.sort().filter(function(elem) { return elem != undefined });
 	}
-	get_english_speaking_cities() {
-		var countriesArray =  this.get_english_speaking_countries()
+	get_english_speaking_cities()
+	{
+		var countriesArray = this.get_english_speaking_countries();
 		
-		var ret = this.cities.map(function(city) {
- 			countriesArray.foreach(function(country) {
-				if (city.country == country.country_name)
-					return city.city_name;
+		var ret = this.cities.map(function(city)
+		{	
+			var newCity;
+			countriesArray.forEach(function(country)
+			{
+				if (city.country == country)
+					newCity = city.city_name;
 			})
+			return newCity;
 		})
-		return ret.sort();
+		return ret.sort().filter(function(elem) { return elem != undefined });
 	}
 }
--- ex02/script.js
+++ ex02/script.js
@@ -12,8 +12,7 @@
 
 // Toutes les fonctions "get" doivent retourner leurs résultats classés par ordres alphabétique.
 
-class geographie {
-
+class geography {
 	constructor() {
 		this.countries = [
 		{
@@ -44,52 +43,130 @@ class geographie {
 			if (country.english)
 				return country.country_name;
 		})
-		return ret.sort();
+		return ret.sort().filter(function(elem) { return elem != undefined });
 	}
-	get_countries_by_continent(continent) 
+	get_countries_by_continent(continent)
 	{
 		var ret = this.countries.map(function(country)
 		{
 			if (country.continent == continent)
 				return country.country_name;
 		})
-		return ret.sort();
+		return ret.sort().filter(function(elem) { return elem != undefined });
 	}
 	get_cities_by_continent(continent)
 	{
 		var countriesArray = this.get_countries_by_continent(continent);
-		
+
 		var ret = this.cities.map(function(city)
 		{
-			countriesArray.foreach(function(country)
+			var newCity;
+			countriesArray.forEach(function(country)
 			{
-				if (city.country == country.country_name)
-					return city.city_name;
+				if (city.country == country)
+					newCity = city.city_name;
 			})
+			return newCity;
 		})
-		return ret.sort();
+		return ret.sort().filter(function(elem) { return elem != undefined });
 	}
 	get_english_speaking_cities()
 	{
-		var countriesArray =  this.get_english_speaking_countries();
+		var countriesArray = this.get_english_speaking_countries();
 		
 		var ret = this.cities.map(function(city)
-		{
-			countriesArray.foreach(function(country)
+		{	
+			var newCity;
+			countriesArray.forEach(function(country)
 			{
-				if (city.country == country.country_name)
-					return city.city_name;
-			}
+				if (city.country == country)
+					newCity = city.city_name;
 			})
+			return newCity;
 		})
-		return ret.sort();
+		return ret.sort().filter(function(elem) { return elem != undefined });
 	}
 }
 
-var geo = new geographie();
+var geo = new geography;
 
-function add_place() {
-	var place = prompt("Quel(le) ville/pays souhaitez-vous ajouter ?");
+function write_english_place(param) {
+	var retCountries = geo.get_english_speaking_countries();
+	var retCities = geo.get_english_speaking_cities();
 
-	console.log(place);
+	if (retCountries[0] == null)
+		console.log("Il n'y aucun(e) pays/ville anglophone enregistré.");
+	else {
+		if (param == "pays")
+			console.log("Les pays anglophones sont : "+retCountries.toString());
+		else if (param == "ville")
+			console.log("Les villes anglophones sont : "+retCities.toString());
+	}
+}
+
+function write_continent_place() {
+	var continent = prompt("Quel continent voulez-vous observer ?");
+	var retCountries = geo.get_countries_by_continent(continent);
+	var retCities = geo.get_cities_by_continent(continent);
+
+	if (retCountries[0] == null)
+		console.log("Il n'y a pas de pays associé à ce continent.");
+	else {
+		console.log("Les pays appartenant à "+continent+" sont : "+retCountries.toString());
+		console.log("Les villes appartenant à "+continent+" sont : "+retCities.toString());
+	}
+}
+
+function add_place(place) {
+	if (place == "ville")
+	{
+		var ret = prompt("Entrez la ville souhaitée sous ce format : nom de la ville, son pays");
+
+		if (ret == null)
+		{
+			console.log("Une erreur s'est produite avec la donnée saisie");
+			return ;
+		}
+		cityArray = ret.split(', ');
+		if (cityArray.length != 2)
+		{
+			console.log("Mauvais format de ville saisie");
+			return ;
+		}
+		cityObjet = {
+			'city_name' : cityArray[0],
+			'country' : cityArray[1]
+		}
+		geo.insert_city(cityObjet);
+	}
+	else if (place == "pays")
+	{
+		var ret = prompt("Entrez le pays souhaité sous ce format : nom du pays, son continent, langue anglaise : oui ou non");
+
+		if (ret == null)
+		{
+			console.log("Une erreur s'est produite avec la donnée saisie");
+			return ;
+		}
+		countryArray = ret.split(', ');
+		if (countryArray.length != 3)
+		{
+			console.log("Mauvais format de pays saisie");
+			return ;
+		}
+		if (countryArray[2] == "oui")
+			countryArray[2] = true;
+		else if (countryArray[2] == "non")
+			countryArray[2] = false;
+		countryObjet = {
+			'country_name' : countryArray[0],
+			'continent' : countryArray[1],
+			'english' : countryArray[2]
+		}
+		geo.insert_country(countryObjet);
+	}
+	String.prototype.capitalize = function() {
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	}
+	console.log(place.capitalize(),"ajouté(e) !");
 }
